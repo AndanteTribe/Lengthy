4039cba2677eaccc579b80ed3bd53d17007f8d27
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-04-24 00:00:28 +0900

    feat: Implement LengthyView for displaying text assets with UI integration

---
+++ src/Lengthy.Unity/Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DefaultNamespace
+{
+    public class Test : MonoBehaviour
+    {
+        [SerializeField]
+        private UIDocument _uiDocument;
+        [SerializeField]
+        private StyleSheet _lengthyUss;
+        [SerializeField]
+        private TextAsset _textAsset;
+
+        private void Start()
+        {
+            var root = _uiDocument.rootVisualElement;
+            root.styleSheets.Add(_lengthyUss);
+            root.Add(new Lengthy.LengthyView(_textAsset, title: "利用規約など"));
+        }
+    }
+}
